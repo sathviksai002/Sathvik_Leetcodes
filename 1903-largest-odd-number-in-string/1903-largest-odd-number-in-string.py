0ee6c3c48528670c40230d664dd4875973d0318e
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2022-02-06 10:27:39 +0530

    Time: 872 ms (7.51%), Space: 15.2 MB (80.83%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -3,6 +3,6 @@ class Solution:
         while num:
             if int(num[-1])%2 == 1: #always check the last digit whenever the remainder is leaving 1 as output its odd
                 return num
-            num = num[:-1]
+            num = num[:-1]           #storing the string as decreasing digit one by one and string
         return ""
         
