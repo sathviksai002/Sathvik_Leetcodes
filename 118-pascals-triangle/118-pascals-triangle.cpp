f266bc592a52ce349e8ec991477a643e8b74c7f6
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2022-09-20 15:27:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (99.34%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal(numRows);
+        for(int i = 0 ; i < numRows; i++){
+            pascal[i].resize(i+1); 
+            pascal[i][0] = pascal[i][i] = 1; // first and last element is 1
+        for(int j = 1 ; j < i ; j++)
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+    }
+        return pascal;
+    }
+};
