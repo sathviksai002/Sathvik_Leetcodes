57d3579110281c1dbb9c40b3145a74b0e15f0b6d
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2022-09-29 17:00:23 +0530

    Time: 11 ms (25.35%), Space: 9.6 MB (5.11%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-         // Iterator used to store the position of searched element
-        std::vector<int>::iterator it;
-        // for(auto i : matrix){
-        //     it = std::find (i.begin(), i.end(), target);
-        //     if (it != i.end()){
-        //         return "true";
-        //         break;
-        //     }
-        // }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int m = matrix[0].size();
+        int i = 0; //first row
+        int j = m-1;
+        bool flag = false;
+        while (i<matrix.size() && j>=0){
+            if (matrix[i][j] == target){
+                flag = true;
+                return flag;
+            }
+            if (matrix[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
             }
         }
-        return false;
+    return flag;
     }
 };
